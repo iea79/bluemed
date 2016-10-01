e22fcab97f3e3c8c6b21d894df454d2c7c5d8dea
Evgeny Ivanov <busforward@gmail.com>
2016-10-01 20:06:04 +0500

    Decopressed styles

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@ http_path = ".."
 css_dir = "css" # by Compass.app 
 sass_dir = "sass" # by Compass.app 
 images_dir = "img" # by Compass.app 
-output_style = :compressed # by Compass.app 
+output_style = :expanded # by Compass.app 
 relative_assets = false # by Compass.app 
 line_comments = false # by Compass.app 
 sass_options = {:debug_info=>false} # by Compass.app 
